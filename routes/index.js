c7d50f9ec710c52a03f988c35127f1eb0642d28f
wpiyong <lylylyly@hotmail.com>
2017-12-18 16:33:10 -0500

    test again

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express New Test' });
+  res.render('index', { title: 'Express New Test again' });
 });
 
 module.exports = router;
