5ec6bcc608858f307e27f6400eb40e0541d2bc08
wpiyong <lylylyly@hotmail.com>
2017-12-19 08:50:29 -0500

    update hook

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express New Test again' });
+  res.render('index', { title: 'Express New Test again and again' });
 });
 
 module.exports = router;
