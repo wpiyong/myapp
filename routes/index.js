5391c82d5691501f8b65ee516c233c52d7a90369
wpiyong <lylylyly@hotmail.com>
2017-12-19 09:00:58 -0500

    new update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express New Test again and again' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
